0d58b21f55e2330b3892ff8e64e4e7be9409e7a0
linmyatmin <lynnmyatminn@gmail.com>
2023-09-15 14:46:29 +0700

    End of section 5

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService;
 using AuctionService.Data;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,8 +31,19 @@ builder.Services.AddMassTransit(x => {
     });
 });
 
+//JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
+app.UseAuthentication();
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
 
